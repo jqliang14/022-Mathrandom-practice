7389196624fd25133efd1659e94ccfb56d4c571a
Jian Q Liang <30308734+jqliang14@users.noreply.github.com>
2021-11-17 17:12:06 -0600

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,12 @@
+// Question 1
+let randomNumber = Math.random();
+console.log(randomNumber);
+
+
+$(".answer1").text(`The random number is... ${randomNumber} `);
+
+
+// Question 2
+
+let randomNumber1 = Math.ceil(Math.random()*4) ;
+$(".anwserTwo" ).text(`The random number is... ${randomNumber}`);
